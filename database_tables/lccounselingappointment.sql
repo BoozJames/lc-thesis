29b364a5f3393edfb15b473aff65bf4651b6fdf1
unknown <jamesboozmanumbale@gmail.com>
2023-01-16 02:43:01 +0800

    updated

--- database_tables/lccounselingappointment.sql
+++ database_tables/lccounselingappointment.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 13, 2022 at 01:11 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.0
+-- Host: localhost:3306
+-- Generation Time: Jan 15, 2023 at 06:42 PM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,7 +28,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `lccounselingappointment` (
-  `id` int(225) NOT NULL,
+  `id` int NOT NULL,
   `file_type` varchar(225) NOT NULL,
   `student_lastname` varchar(225) NOT NULL,
   `student_firstname` varchar(225) NOT NULL,
@@ -36,18 +36,18 @@ CREATE TABLE `lccounselingappointment` (
   `student_year` varchar(225) NOT NULL,
   `student_course_section` varchar(225) NOT NULL,
   `student_number` varchar(225) NOT NULL,
-  `filename` varchar(225) NOT NULL,
+  `main_concern` varchar(3000) NOT NULL,
+  `remarks` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
   `status` varchar(225) NOT NULL,
-  `submission_date` varchar(225) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `schedule_date` varchar(225) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `lccounselingappointment`
 --
 
-INSERT INTO `lccounselingappointment` (`id`, `file_type`, `student_lastname`, `student_firstname`, `student_middlename`, `student_year`, `student_course_section`, `student_number`, `filename`, `status`, `submission_date`) VALUES
-(1, 'Peer Facilitator Group Application Form', 'Peer Facilitator Group Application Form', 'Peer Facilitator Group Application Form', 'student_middlename', 'Peer Facilitator Group Application Form', 'Peer Facilitator Group Application Form', 'Peer Facilitator Group Application Form', 'Chapter-1-2-revised.pdf', '', ''),
-(2, 'Peer Facilitator Group Application Form', 'Peer Facilitator Group Application Form', 'Peer Facilitator Group Application Form', 'student_middlename', 'Peer Facilitator Group Application Form', 'Peer Facilitator Group Application Form', 'Peer Facilitator Group Application Form', 'Chapter-1-2-revised.pdf', '', '');
+INSERT INTO `lccounselingappointment` (`id`, `file_type`, `student_lastname`, `student_firstname`, `student_middlename`, `student_year`, `student_course_section`, `student_number`, `main_concern`, `remarks`, `status`, `schedule_date`) VALUES
+(3, 'Appointment for Counseling', 'Juana', 'Dela Cruz', '', '4th', 'BSIT', '1001', 'Main Concern of Juana', '', '', '2023-01-16');
 
 --
 -- Indexes for dumped tables
@@ -67,7 +67,7 @@ ALTER TABLE `lccounselingappointment`
 -- AUTO_INCREMENT for table `lccounselingappointment`
 --
 ALTER TABLE `lccounselingappointment`
-  MODIFY `id` int(225) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
