0fb7befb034f6875c8ca9aa0ec1374bde4fb2a68
unknown <jamesboozmanumbale@gmail.com>
2023-01-15 17:45:13 +0800

    updated table

--- database_tables/lcadmintable.sql
+++ database_tables/lcadmintable.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Generation Time: Nov 13, 2022 at 01:02 AM
--- Server version: 10.4.22-MariaDB
--- PHP Version: 8.1.0
+-- Host: localhost:3306
+-- Generation Time: Jan 15, 2023 at 09:44 AM
+-- Server version: 8.0.30
+-- PHP Version: 8.1.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,21 +28,24 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `lcadmintable` (
-  `id` int(225) NOT NULL,
+  `id` int NOT NULL,
   `admin_type` varchar(225) NOT NULL,
   `admin_name` varchar(225) NOT NULL,
   `admin_age` varchar(225) NOT NULL,
   `admin_gender` varchar(225) NOT NULL,
   `admin_username` varchar(225) NOT NULL,
-  `admin_password` varchar(225) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `admin_password` varchar(225) NOT NULL,
+  `is_approved` tinyint(1) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Dumping data for table `lcadmintable`
 --
 
-INSERT INTO `lcadmintable` (`id`, `admin_type`, `admin_name`, `admin_age`, `admin_gender`, `admin_username`, `admin_password`) VALUES
-(1, 'Guidance Admin', 'sample admin', '23', 'male', 'sampleadmin', 'samplepass');
+INSERT INTO `lcadmintable` (`id`, `admin_type`, `admin_name`, `admin_age`, `admin_gender`, `admin_username`, `admin_password`, `is_approved`) VALUES
+(1, 'Guidance Admin', 'sample admin', '23', 'male', 'sampleadmin', 'samplepass', 1),
+(2, 'Student', 'Juana Dela Cruz', '22', 'Female', 'juana', 'juana', 0),
+(3, 'Student', 'test', 'test', 'test', 'test', 'tes', 0);
 
 --
 -- Indexes for dumped tables
@@ -62,7 +65,7 @@ ALTER TABLE `lcadmintable`
 -- AUTO_INCREMENT for table `lcadmintable`
 --
 ALTER TABLE `lcadmintable`
-  MODIFY `id` int(225) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
