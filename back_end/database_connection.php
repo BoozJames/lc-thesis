f0d88a577fbf287b007bc39fdbada7557080fa76
unknown <jamesboozmanumbale@gmail.com>
2023-01-22 06:00:54 +0800

    update connection

--- back_end/database_connection.php
+++ back_end/database_connection.php
@@ -1,6 +1,6 @@
 <?php
 
-$dbConnection = mysqli_connect("217.21.74.101","root", "","0q+1$0p5u3H/");
+$dbConnection = mysqli_connect("u187580921_lcthesis","u187580921_root", "","0q+1$0p5u3H/");
 	if(mysqli_connect_errno()){
 		echo "Failed to connect to MySQL: " . mysqli_connect_error() ;
 	}else{
